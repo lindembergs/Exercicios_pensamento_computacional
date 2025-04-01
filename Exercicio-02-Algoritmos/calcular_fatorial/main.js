59305b084abdf3df22765aad50d17f10e5ef7207
Lindemberg <lindembergparaiba@gmail.com>
2025-04-01 16:23:55 -0300

    Calcular fatorial feito

---
+++ Exercicio-02-Algoritmos/calcular_fatorial/main.js
@@ -0,0 +1,19 @@
+// 5. Escrever um algoritmo que lê um valor inteiro, calcula o fatorial desse número e
+// mostra o resultado.
+const calculaFatorial = () => {
+  let numero;
+  do {
+    numero = Number(window.prompt("Digite um número inteiro e positivo"));
+    if (numero == 0 || numero == 1) {
+      alert(`O fatorial de ${numero} é: 1`);
+      return;
+    }
+  } while (isNaN(numero) || numero < 0);
+  let resultado = 1;
+  for (let index = 1; index <= numero; index++) {
+    resultado *= index;
+  }
+  alert(`O fatorial de ${numero} é: ${resultado}`);
+};
+
+calculaFatorial();
