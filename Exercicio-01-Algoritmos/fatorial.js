dd848f8e48021dee3dc73d7f327f20431581a5c8
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:29:08 -0300

    refactor: fatorial migrado para console

--- Exercicio-01-Algoritmos/fatorial.js
+++ Exercicio-01-Algoritmos/fatorial.js
@@ -1,11 +1,12 @@
 // 5. Escrever um algoritmo que lê um valor inteiro, calcula o fatorial desse número e
 // mostra o resultado.
+const prompt = require("prompt-sync")();
 const calculaFatorial = () => {
   let numero;
   do {
-    numero = Number(window.prompt("Digite um número inteiro e positivo"));
+    numero = Number(prompt("Digite um número inteiro e positivo"));
     if (numero == 0 || numero == 1) {
-      alert(`O fatorial de ${numero} é: 1`);
+      console.log(`O fatorial de ${numero} é: 1`);
       return;
     }
   } while (isNaN(numero) || numero < 0);
@@ -13,7 +14,7 @@ const calculaFatorial = () => {
   for (let index = 1; index <= numero; index++) {
     resultado *= index;
   }
-  alert(`O fatorial de ${numero} é: ${resultado}`);
+  console.log(`O fatorial de ${numero} é: ${resultado}`);
 };
 
 calculaFatorial();
