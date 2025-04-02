ef6ad66e8c34e5d72b6f060cfe4d079003dd9b4c
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:39:20 -0300

    refactor: soma repetida  migrado para console

--- Exercicio-01-Algoritmos/soma_repetida.js
+++ Exercicio-01-Algoritmos/soma_repetida.js
@@ -1,15 +1,15 @@
 // 7. Crie um algoritmo que recebe 2 números e multiplica o num1 pelo num2 através de
 // somas repetidas. (ex: 2 e 3 = 2 + 2 + 2)
-
+const prompt = require("prompt-sync")();
 const calcularComSomaRepetida = () => {
   let n1, n2, resultado;
   resultado = 0;
-  n1 = Number(window.prompt("Digite o primeiro número"));
-  n2 = Number(window.prompt("Digite o segundo número"));
+  n1 = Number(prompt("Digite o primeiro número"));
+  n2 = Number(prompt("Digite o segundo número"));
   for (let i = 0; i < n2; i++) {
     resultado += n1;
   }
-  alert(resultado);
+  console.log(resultado);
 };
 
 calcularComSomaRepetida();
