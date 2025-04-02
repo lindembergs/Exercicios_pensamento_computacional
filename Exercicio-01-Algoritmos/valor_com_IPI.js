a925e81159867111b32d99f9b6a47bbac49518e4
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:40:26 -0300

    refactor: valor com IPI  migrado para console

--- Exercicio-01-Algoritmos/valor_com_IPI.js
+++ Exercicio-01-Algoritmos/valor_com_IPI.js
@@ -4,7 +4,7 @@
 // - o código da peça 2, valor unitário da peça 2, quantidade de peças 2
 // O algoritmo deve calcular o valor total a ser pago e apresentar o resultado.
 // Fórmula : (valor1*quant1 + valor2*quant2)*(IPI/100 + 1)
-
+const prompt = require("prompt-sync")();
 const calculaValorComIPI = () => {
   let codigoPeca1,
     codigoPeca2,
@@ -15,43 +15,18 @@ const calculaValorComIPI = () => {
     IPI,
     valorTotalSemIPI,
     valorTotalComIPI;
-  codigoPeca1 = Number(window.prompt("Digite o código da primeira peça"));
-  valorDapeca1 = Number(window.prompt("Digite o valor da primeira peça"));
-  quantidadeDepecas1 = Number(window.prompt("Digite a quantidade de peças"));
-  codigoPeca2 = Number(window.prompt("Digite o código da segunda peça"));
-  valorDapeca2 = Number(window.prompt("Digite o valor da segunda peça"));
-  quantidadeDepecas2 = Number(window.prompt("Digite a quantidade de peças"));
-  IPI = Number(window.prompt("Digite o valor do IPI"));
+  codigoPeca1 = Number(prompt("Digite o código da primeira peça"));
+  valorDapeca1 = Number(prompt("Digite o valor da primeira peça"));
+  quantidadeDepecas1 = Number(prompt("Digite a quantidade de peças"));
+  codigoPeca2 = Number(prompt("Digite o código da segunda peça"));
+  valorDapeca2 = Number(prompt("Digite o valor da segunda peça"));
+  quantidadeDepecas2 = Number(prompt("Digite a quantidade de peças"));
+  IPI = Number(prompt("Digite o valor do IPI"));
   IPI += IPI / 100 + 1;
   valorTotalSemIPI =
     valorDapeca1 * quantidadeDepecas1 + valorDapeca2 * quantidadeDepecas2;
   valorTotalComIPI = valorTotalSemIPI + IPI;
-  // Os códigos da linha 30 a 52 é funcional, porém desnecessário para o exercício, feito para fins de aprendizado
-  // const pecas = [
-  //   {
-  //     peca1: {
-  //       codigo: codigoPeca1,
-  //       valor: valorDapeca1,
-  //       quantidadeDepecas: quantidadeDepecas1,
-  //     },
-  //     peca2: {
-  //       codigo: codigoPeca2,
-  //       valor: valorDapeca2,
-  //       quantidadeDepecas: quantidadeDepecas2,
-  //     },
-  //   },
-  // ];
-  // const divRef = document.querySelector(".caixa");
-
-  // pecas.map((peca) => {
-  //   let tagp = document.createElement("p");
-  //   console.log(peca);
-  //   // tagp.innerText = JSON.stringify(peca, null, 2);
-  //   tagp.innerText = `Código da primeira peça: ${peca.peca1.codigo}, preço da primeira peça: ${peca.peca1.valor}, quantidade da primeira peça: ${peca.peca1.quantidadeDepecas} <br> Código da segunda peça: ${peca.peca1.codigo}, preço da segunda peça: ${peca.peca1.valor}, quantidade da segunda peça: ${peca.peca1.quantidadeDepecas}, `;
-  //   divRef.appendChild(tagp);
-  // });
-
-  alert(
+  console.log(
     `O valor total das peças sem IPI é: ${valorTotalSemIPI}, e o valor total com a adição do IPI é: ${valorTotalComIPI}`
   );
 };
