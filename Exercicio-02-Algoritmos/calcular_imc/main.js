2e642fc388e6850ce2b342a6689530ffd8c89995
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 11:39:03 -0300

    Calcular IMC funcionando

---
+++ Exercicio-02-Algoritmos/calcular_imc/main.js
@@ -0,0 +1,59 @@
+// 2 - Criar um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa
+// e exiba a classificação (abaixo do peso, peso normal, sobrepeso, obesidade).
+
+// IMC (kg/m²)	Classificação
+// Menor que 18,5	Abaixo do peso
+// 18,5 - 24,9	Peso normal
+// 25,0 - 29,9	Sobrepeso
+// 30,0 - 34,9	Obesidade grau 1
+// 35,0 - 39,9	Obesidade grau 2 (severa)
+// Maior ou igual a 40	Obesidade grau 3 (mórbida)
+const calcularIMC = () => {
+  let peso, altura, valorIMC;
+  peso = Number(window.prompt("Digite o seu peso"));
+  altura = Number(window.prompt("Digite sua altura separado por . ex: 1.72"));
+  valorIMC = peso / (altura * altura);
+  if (valorIMC < 18.5) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: < 18,5 e está abaixo do peso `
+    );
+  }
+  if (valorIMC == 18.5 && valorIMC <= 24.9) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: 18,5 - 24,9	Peso normal `
+    );
+  }
+  if (valorIMC >= 25 && valorIMC <= 29.9) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: 25,0 - 29,9	Sobrepeso `
+    );
+  }
+  if (valorIMC >= 30 && valorIMC <= 34.9) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: 30,0 - 34,9	Obesidade grau 1 `
+    );
+  }
+  if (valorIMC >= 35 && valorIMC <= 39.9) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: 35,0 - 39,9	Obesidade grau 2 (severa) `
+    );
+  }
+  if (valorIMC >= 40) {
+    alert(
+      `Seu IMC é: ${valorIMC.toFixed(
+        2
+      )}, você faz parte da classificação: Maior ou igual a 40	Obesidade grau 3 (mórbida) `
+    );
+  }
+};
+calcularIMC();
