858897609036428f6b4454e3ad3360c02ec0533d
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:41:22 -0300

    refactor: idade  migrado para console

--- Exercicio-02-Algoritmos/idade.js
+++ Exercicio-02-Algoritmos/idade.js
@@ -1,19 +1,19 @@
 // 1 - Criar um algoritmo que leia a idade de uma pessoa e informe se ela é criança (0-12),
 // adolescente (13-17), adulta (18-59) ou idosa (60+).
-
+const prompt = require("prompt-sync")();
 const calcularIdade = () => {
-  let idade = Number(window.prompt("Informe sua idade"));
+  let idade = Number(prompt("Informe sua idade"));
   if (idade <= 12) {
-    alert(`Você possui ${idade} anos então é uma criança`);
+    console.log(`Você possui ${idade} anos então é uma criança`);
   }
   if (idade > 12 && idade <= 17) {
-    alert(`Você possui ${idade} anos então é adolescente`);
+    console.log(`Você possui ${idade} anos então é adolescente`);
   }
   if (idade > 17 && idade <= 59) {
-    alert(`Você possui ${idade} anos então é uma pessoa adulta`);
+    console.log(`Você possui ${idade} anos então é uma pessoa adulta`);
   }
   if (idade > 59) {
-    alert(`Você possui ${idade} anos então é uma pessoa idosa`);
+    console.log(`Você possui ${idade} anos então é uma pessoa idosa`);
   }
 };
 calcularIdade();
