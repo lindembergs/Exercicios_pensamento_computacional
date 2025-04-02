a5e9660ee26ecb988183838a9bc3bb609196e1d9
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:38:39 -0300

    refactor: positivo e negativo  migrado para console

--- Exercicio-01-Algoritmos/positivo_negativo.js
+++ Exercicio-01-Algoritmos/positivo_negativo.js
@@ -1,22 +1,20 @@
 // 1. Crie um algoritmo que leia um número diferente de zero e diga se este número é
 // positivo ou negativo.
-
-// Obs, é necessário rodar a aplicação no navegador.
-
-const numero = window.prompt("Digite um número inteiro diferente de 0");
+const prompt = require("prompt-sync")();
+const numero = prompt("Digite um número inteiro diferente de 0");
 const numeroConvertido = BigInt(numero);
 const verificaNumeroMaiorOuMenor = (numeroConvertido) => {
   while (numeroConvertido == 0) {
-    alert(
+    console.log(
       `O número digitado foi ${numeroConvertido}, por favor, digite um número diferente`
     );
-    numeroConvertido = BigInt(window.prompt("Digite um número diferente de 0"));
+    numeroConvertido = BigInt(prompt("Digite um número diferente de 0"));
   }
   if (numeroConvertido > 0) {
-    alert(`O número digitado foi ${numeroConvertido} e é positivo`);
+    console.log(`O número digitado foi ${numeroConvertido} e é positivo`);
   }
   if (numeroConvertido < 0) {
-    alert(`O número digitado foi ${numeroConvertido} e é negativo`);
+    console.log(`O número digitado foi ${numeroConvertido} e é negativo`);
   }
 };
 verificaNumeroMaiorOuMenor(numero);
