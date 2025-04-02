48ad21b5dcfd8c8288cc0b5dd381f81c64a7f698
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:42:59 -0300

    refactor: calcula valor das parcelas migrado para console

--- Exercicio-02-Algoritmos/parcelas.js
+++ Exercicio-02-Algoritmos/parcelas.js
@@ -1,14 +1,12 @@
 // 3 - Criar um algoritmo que leia o valor de uma compra e o número de parcelas. Em seguida, calcule e exiba o valor de cada parcela.
-
+const prompt = require("prompt-sync")();
 const calcularValorDasParcelas = () => {
   let valorDaCompra, quantidadeDeParcelas, valorDeCadaParcela;
-  valorDaCompra = Number(window.prompt("Digite o valor da compra"));
-  quantidadeDeParcelas = Number(
-    window.prompt("Digite a quantidade de parcelas")
-  );
+  valorDaCompra = Number(prompt("Digite o valor da compra"));
+  quantidadeDeParcelas = Number(prompt("Digite a quantidade de parcelas"));
 
   valorDeCadaParcela = valorDaCompra / quantidadeDeParcelas;
-  alert(
+  console.log(
     `O valor da compra é ${valorDaCompra}, a quantidade de parcelas escolhida foi: ${quantidadeDeParcelas}. O valor de cada parcela é: ${valorDeCadaParcela}`
   );
 };
