e37594bf61de77e9ff84c2d47bbcb94a5d096924
Lindemberg <lindembergparaiba@gmail.com>
2025-04-02 14:36:28 -0300

    refactor: maior e menor  migrado para console

--- Exercicio-01-Algoritmos/maior_menor.js
+++ Exercicio-01-Algoritmos/maior_menor.js
@@ -1,28 +1,31 @@
-// 2. Crie um algoritmo que receba 3 números e informe qual o maior entre eles.
+const prompt = require("prompt-sync")();
 
-let n1, n2, n3;
 const verificaMaiorMenor = () => {
-  n1 = Number(window.prompt("Digite o primeiro número"));
-  n2 = Number(window.prompt("Digite o segundo número"));
-  n3 = Number(window.prompt("Digite o terceiro número"));
-  if ((n1 == 0, n2 == 0, n3 == 0)) {
-    alert("os 3 números digitados são 0");
+  let n1, n2, n3;
+
+  do {
+    n1 = Number(prompt("Digite o primeiro número: "));
+    n2 = Number(prompt("Digite o segundo número: "));
+    n3 = Number(prompt("Digite o terceiro número: "));
+
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
+      console.log("Por favor, digite 3 números válidos.");
+    }
+  } while (isNaN(n1) || isNaN(n2) || isNaN(n3));
+
+  if (n1 === 0 && n2 === 0 && n3 === 0) {
+    console.log("Os 3 números digitados são 0.");
+    return;
   }
-  if ((n1 == n2, n1 == n3)) {
-    alert("os 3 números digitados são iguais");
-  } else {
-    do {
-      n1 = Number(window.prompt("Digite o primeiro número"));
-      n2 = Number(window.prompt("Digite o segundo número"));
-      n3 = Number(window.prompt("Digite o terceiro número"));
-      if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
-        alert("Por favor, digite 3 números inteiros válidos.");
-      }
-    } while (isNaN(n1) || isNaN(n2) || isNaN(n3));
-    let maior = Math.max(n1, n2, n3);
-    let menor = Math.min(n1, n2, n3);
-    alert(`O maior número é ${maior} e o menor número é ${menor}`);
+
+  if (n1 === n2 && n1 === n3) {
+    console.log("Os 3 números digitados são iguais.");
+    return;
   }
+
+  let maior = Math.max(n1, n2, n3);
+  let menor = Math.min(n1, n2, n3);
+  console.log(`O maior número é ${maior} e o menor número é ${menor}`);
 };
 
 verificaMaiorMenor();
