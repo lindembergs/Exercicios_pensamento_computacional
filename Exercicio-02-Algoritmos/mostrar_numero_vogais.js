cd84692938e37cf4d90091164cc28b83999363f4
Lindemberg <lindembergparaiba@gmail.com>
2025-04-03 10:57:55 -0300

    Mostrar quantidade de vogais em uma palavra funcionando

---
+++ Exercicio-02-Algoritmos/mostrar_numero_vogais.js
@@ -0,0 +1,17 @@
+// 14- Criar um algoritmo que leia uma frase e exiba o total de vogais presentes nela.
+const prompt = require("prompt-sync")();
+
+const exibirTotalDeVogais = () => {
+  const vogais = ["a", "e", "i", "o", "u"];
+  let palavra, palavraSplit;
+  palavra = prompt("Digite uma palavra ").toLocaleLowerCase;
+  palavraSplit = palavra.split("");
+  // console.log(palavraSplit);
+  let quantidadeVogais = palavraSplit.filter((letra) => vogais.includes(letra));
+  console.log("Vogais encontradas:", quantidadeVogais);
+  console.log(
+    `A quantidade de vogais na palavra ${palavra} é: ${quantidadeVogais.length}`
+  );
+};
+
+exibirTotalDeVogais();
